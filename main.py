d6f0bc81b9144cbe017aefd96632c7afb81ba4e4
Randy wahidin <63757267+Randi356@users.noreply.github.com>
2021-09-17 09:17:05 +0700

    Update main.py

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ TGraph = Client(
 @TGraph.on_message(filters.command("start"))
 async def start(client, message):
     await message.reply_text(
-        f"Hello {message.from_user.first_name},\nIm telegram to telegra.ph image uploader bot by ⚡ ⚡ Sithum Batrow 🇱🇰⚡⚡ @sibatrow",
+        f"Hello {message.from_user.first_name},\nIm telegram to telegra.ph image uploader bot by 🔷️ alexander 🔷️ @FlashProSpeed",
         True,
     )
 
